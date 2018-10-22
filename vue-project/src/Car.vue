6d037cfffdc87dd96d510067c56866ef30352a89
Julia Vangeliy <vangeliy_yulia@mail.ru>
2018-10-22 19:27:57 +0300

    Основы

---
+++ vue-project/src/Car.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h3>Name: {{ carName }}</h3>
+        <p>Year: {{ carYear }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['carName', 'carYear']
+}
+</script>
+
+
